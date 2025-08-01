170e1a46da024d4042f3a7b4545f3d2a343b0adc
Wäinö Lehmonen <waiski.lehmonen@gmail.com>
2025-08-01 11:12:40 +0300

    Pyyttonikoodi

---
+++ FFTdeltak0.py
@@ -0,0 +1,163 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 16 07:42:21 2025
+
+@author: Wäinö Lehmonen
+"""
+
+import scipy as sc
+import numpy as np
+import matplotlib.pyplot as plt
+import random
+
+
+# constants (m/H <= 3/2 for real nu; for plotting pick m/H = small like 0.01)
+m = 0.01  # mass of field
+H = 1  # Hubble rate
+d = 4  # dimension
+
+nu = np.sqrt(((d - 1)/2)**2 - (m/H)**2)
+
+k = 10  # norm of k bar in [0, inf)
+etabar = -0.1  # mean of conformal times 1/2(eta' + eta) in (-inf, 0)
+# -k*etabar > 1 subhorizon
+# -k*etabar < 1 superhorizon
+
+
+# number of data points for plotting and FFT
+N = 1000
+# integration variable u = Delta_eta/(2eta_bar) runs over (-1, 1)
+epsilon = 1e-10  # small threshold in order to avoid limit points (nan)
+urange = np.linspace(-1 + epsilon, 1 - epsilon, N)
+
+# for variable k0
+L = 15  # limits for k0 (when etabar = -1)
+k0range = np.linspace(L/etabar + k, -L/etabar + k, N)
+# number of data points can be adjusted (then FFT matrix is no longer square)
+
+
+def integrand_for_Delta(k_0, eta_bar, u):
+    """
+    This integrated over u in (-1, 1) should be Delta^<_k
+
+    Parameters
+    ----------
+    k_0 : array-like
+        frequency
+    eta_bar : array-like
+        mean of conformal times 1/2(eta' + eta)
+    u : array-like
+        Integration variable u = (Deltaeta)/etabar;
+        Deltaeta = eta' - eta: conformal time difference
+
+    Returns
+    -------
+    array-like
+        Integrand for given values
+
+    """
+    hankel_a = sc.special.hankel1(nu, k*eta_bar*(u - 1))
+    hankel_b = sc.special.hankel2(nu, -k*eta_bar*(u + 1))
+    c = (1 - u**2)**(3/2)*np.exp(-1j*k_0*2*u*eta_bar)
+    return np.pi/2*H**2*eta_bar**4*c*hankel_a*hankel_b
+
+
+# Integrals using scipy quad
+# for variable k0
+def Delta_benchmark_integral(k_0, eta_bar):
+    I_Re = lambda u: integrand_for_Delta(k_0, eta_bar, u).real
+    I_Im = lambda u: integrand_for_Delta(k_0, eta_bar, u).imag
+    return sc.integrate.quad(I_Re, -1, 1)[0] \
+        - 1j*sc.integrate.quad(I_Im, -1, 1)[0]
+
+
+integral_k = Delta_benchmark_integral(k, etabar)
+# print(benchmark_integral)
+
+
+integrand_k = integrand_for_Delta(k, etabar, urange)  # integrand at given points u
+# # this benchmark (for FFT) uses same u grid as the FFT (compare with scipy)
+# benchmark_integral1 = sum(integrand_k)*(urange[1]-urange[0])  # value of integral
+# # print(benchmark_integral)
+
+
+def plot_integrand(k_0, eta_bar):
+    # Plotting the integrand for fixed value k0
+    integrand_k0 = integrand_for_Delta(k_0, eta_bar, urange)
+    plt.figure()
+    plt.plot(urange, integrand_k0, 'r')  # plots the real part of I
+    # plt.plot(urange, integrand_k0.imag, 'b')  # imaginary part of I
+    plt.title('Integrand as a function of integration variable u')
+    plt.xlabel(r'$u=\frac{\Delta \eta}{2\bar{\eta}}$')
+    plt.ylabel(r'$I(u, \bar{\eta}, k_0)$')
+    plt.show()
+
+
+# input matrix for FFT
+x1, y1 = np.meshgrid(urange, k0range)
+# FFT matrix
+FFTgrid = np.exp(-1j*x1*2*y1*etabar)*(urange[1]-urange[0])
+
+
+# FFT using the above defined grid
+def FFT_for_G(eta_bar, u):
+    """
+    Fourier transform of G wrt. u
+
+    Parameters
+    ----------
+    eta_bar : array-like
+        mean of conformal times 1/2(eta' + eta)
+    u : array-like
+        Integration variable u = Deltaeta/etabar;
+        Deltaeta = eta' - eta: conformal time difference
+
+    Returns
+    -------
+    array-like
+        Fourier-transform for given values
+
+    """
+    hankel_a = sc.special.hankel1(nu, k*eta_bar*(u - 1))
+    hankel_b = sc.special.hankel2(nu, -k*eta_bar*(u + 1))
+    c = (1 - u**2)**(3/2)
+    A = hankel_a*hankel_b*c
+    return np.pi/2*H**2*eta_bar**4*np.matmul(FFTgrid, A)
+
+
+# Plotting the FFT
+Delta = FFT_for_G(etabar, urange)
+
+
+# testataan onko delta funktio
+k0Delta = Delta*k0range
+integral1 = sum(Delta)*(k0range[1]-k0range[0])
+print(integral1)
+integral2 = sum(k0Delta)*(k0range[1]-k0range[0])
+print(integral2)
+
+
+plt.figure(figsize=(11, 6))
+
+plt.plot(k0range, Delta, 'r', label=r'$\Delta^<_\bar{k}(k_0, \bar{\eta})$ FFT')
+
+plt.title(r'$\Delta^<_\bar k(k_0, \bar \eta)$ as a function of $k_0$'
+          r' when $\bar \eta={a},\ k={b}$'.format(a=etabar, b=k))
+plt.xlabel(r'$k_0 \in \left[k + L/\bar\eta, k - L/\bar\eta \right],\ L={a}$'
+           .format(a=L))
+
+#plt.axvline(0, c='gray')  # comment out when |etabar| is large
+plt.axhline(0, c='gray')
+
+plt.axvline(k, c='b', linestyle='--')
+plt.plot(k, integral_k, 'bD',
+         label=r'$\Delta^<_\bar{k}(k, \bar{\eta})$')
+
+# benchmarking for FFT
+for i in range(5):
+    randk0 = random.choice(k0range)
+    plt.plot(randk0, Delta_benchmark_integral(randk0, etabar), 'go')
+
+plt.legend(loc='upper left', fontsize=13, shadow=True)
+plt.grid()
+plt.show()
